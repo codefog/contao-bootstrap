d6fe84d45ba920e8eb56b0fe6c064465ed1a9db6
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2016-04-22 08:57:30 +0200

    Added new templates

--- src/Resources/contao/config/autoload.php
+++ src/Resources/contao/config/autoload.php
@@ -4,5 +4,21 @@
  * Register the templates
  */
 TemplateLoader::addFiles([
+    // Forms
+    'form_checkbox'     => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/forms',
+    'form_radio'        => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/forms',
+    'form_select'       => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/forms',
+    'form_submit'       => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/forms',
+    'form_textarea'     => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/forms',
+    'form_textfield'    => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/forms',
+
+    // Frontend pages
     'fe_page_bootstrap' => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/frontend',
+
+    // Modules
+    'mod_breadcrumb'    => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/modules',
+    'mod_search'        => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/modules',
+
+    // Pagination
+    'pagination'        => 'vendor/codefog/contao-bootstrap/src/Resources/contao/templates/pagination',
 ]);
